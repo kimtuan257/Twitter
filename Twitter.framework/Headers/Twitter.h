b652b318952d482d4c8e14a2f1b6130d4c60ed03
Tuan Le <tuanlk@tech.est-rouge.com>
2019-09-30 17:02:57 +0700

    Initial SDK

---
+++ Twitter.framework/Headers/Twitter.h
@@ -0,0 +1,19 @@
+//
+//  Twitter.h
+//  Twitter
+//
+//  Created by Phan Nghi on 6/3/19.
+//  Copyright © 2019 Phan Nghi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Twitter.
+FOUNDATION_EXPORT double TwitterVersionNumber;
+
+//! Project version string for Twitter.
+FOUNDATION_EXPORT const unsigned char TwitterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Twitter/PublicHeader.h>
+
+
